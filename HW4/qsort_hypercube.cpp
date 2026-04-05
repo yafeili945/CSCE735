8194fd331fa7bcba2b0fc70e5907f01518e02848
yafeili945 <yafeili@tamu.edu>
2026-04-05 17:19:32 -0500

    Finished HW4 with 9 MPI calls in qsort_hypercube.cpp

--- HW4/qsort_hypercube.cpp
+++ HW4/qsort_hypercube.cpp
@@ -297,7 +297,7 @@ void HyperCube_Class::HyperCube_QuickSort() {
 	// compute the sum of local_median values on processes of this hypercube
 
 	// ***** Add MPI call here *****
-
+	MPI_Allreduce(&local_median, &pivot, 1, MPI_INT, MPI_SUM, sub_hypercube_comm);
 
 	pivot = pivot/sub_hypercube_size;
 
@@ -317,25 +317,25 @@ void HyperCube_Class::HyperCube_QuickSort() {
 	    // MPI-2: Send number of elements greater than pivot
 
 	    // ***** Add MPI call here *****
-
+		MPI_Send(&list_size_gt, 1, MPI_INT, nbr_k, tag, MPI_COMM_WORLD);
 
 	    // MPI-3: Receive number of elements less than or equal to pivot
 
 	    // ***** Add MPI call here *****
-
+		MPI_Recv(&nbr_list_size, 1, MPI_INT, nbr_k, tag, MPI_COMM_WORLD, &status);
 
 	    // Allocate storage for neighbor's list
 	    nbr_list = new int[nbr_list_size];
 
 	    // MPI-4: Send list[idx ... list_size-1] to neighbor
-
+		MPI_Send(&list[idx], list_size_gt, MPI_INT, nbr_k, tag, MPI_COMM_WORLD);
 	    // ***** Add MPI call here *****
 
 
 	    // MPI-5: Receive neighbor's list of elements that are less than or equal to pivot
 
 	    // ***** Add MPI call here *****
-
+		MPI_Recv(&nbr_list[0], nbr_list_size, MPI_INT, nbr_k, tag, MPI_COMM_WORLD, &status);
 
 	    // Merge local list of elements less than or equal to pivot with neighbor's list
 	    new_list = merged_list(list, idx, nbr_list, nbr_list_size); 
@@ -350,12 +350,13 @@ void HyperCube_Class::HyperCube_QuickSort() {
 	    // MPI-6: Receive number of elements greater than pivot
 
 	    // ***** Add MPI call here *****
+		MPI_Recv(&nbr_list_size, 1, MPI_INT, nbr_k, tag, MPI_COMM_WORLD, &status);
 
 
 	    // MPI-7: Send number of elements less than or equal to pivot
 
 	    // ***** Add MPI call here *****
-
+		MPI_Send(&list_size_leq, 1, MPI_INT, nbr_k, tag, MPI_COMM_WORLD);
 
 	    // Allocate storage for neighbor's list
 	    nbr_list = new int[nbr_list_size]; 
@@ -363,12 +364,12 @@ void HyperCube_Class::HyperCube_QuickSort() {
 	    // MPI-8: Receive neighbor's list of elements that are greater than the pivot
 
 	    // ***** Add MPI call here *****
-
+		MPI_Recv(&nbr_list[0], nbr_list_size, MPI_INT, nbr_k, tag, MPI_COMM_WORLD, &status);
 
 	    // MPI-9: Send list[0 ... idx-1] to neighbor
 
 	    // ***** Add MPI call here *****
-
+		MPI_Send(&list[0], list_size_leq, MPI_INT, nbr_k, tag, MPI_COMM_WORLD);
 
 	    // Merge local list of elements greater than pivot with neighbor's list
 	    new_list = merged_list(&list[idx], list_size_gt, nbr_list, nbr_list_size); 
